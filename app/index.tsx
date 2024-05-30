0004cae6acb978f30733b9449f7dd88839d067c7
Jan David <jdno@jdno.dev>
2024-05-30 22:15:32 +0200

    Work-in-progress for authentication with Supabase
    Supabase provides authentication out-of-the-box. Its documentation
    features an in-depth guide for React Native, which we have tried to
    follow to set up the components. But we cannot get the values from
    the configuration yet, so the app does not work.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import "react-native-url-polyfill/auto";
+import Auth from "@/components/Auth";
+import { Session } from "@supabase/supabase-js";
+import { View, Text } from "react-native";
+import { supabase } from "@/lib/supabase";
+import { useState, useEffect } from "react";
 
-export default function Index() {
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Auth />
+      {session && session.user && <Text>{session.user.id}</Text>}
     </View>
   );
 }
