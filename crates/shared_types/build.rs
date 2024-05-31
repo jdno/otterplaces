72b0a37a6121c529ef180712d7c6755b76123dce
Jan David <jdno@jdno.dev>
2024-05-31 08:38:28 +0200

    Set up crates for crux's example (#23)
    We are following the getting started guide[^1] of crux to get the
    development environment set up. The first part of the guide sets up
    various crates, which has been done. The second part, which sets up an
    iOS project, will follow in another commit.
    
    Additionally, we intend to remove most of the example code once we have
    it working. At that point, we will also set up proper tooling to format,
    lint, and test the app.
    
    [^1]: https://redbadger.github.io/crux/getting_started/core.html

---
+++ crates/shared_types/build.rs
@@ -0,0 +1,23 @@
+use std::path::PathBuf;
+
+use crux_core::typegen::TypeGen;
+use shared::Counter;
+
+fn main() -> anyhow::Result<()> {
+    println!("cargo:rerun-if-changed=../shared");
+
+    let mut gen = TypeGen::new();
+
+    gen.register_app::<Counter>()?;
+
+    let output_root = PathBuf::from("./generated");
+
+    gen.swift("SharedTypes", output_root.join("swift"))?;
+
+    gen.java(
+        "com.otterplaces.otterplaces.shared_types",
+        output_root.join("java"),
+    )?;
+
+    Ok(())
+}
---
+++ crates/shared_types/src/lib.rs
@@ -0,0 +1,1 @@
+//! Auto-generated types for Java and Swift
